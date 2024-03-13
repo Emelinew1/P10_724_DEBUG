b54e818e938bba3b25345f75e6349604afd1e383
emelinew1 <emeline.wulstecke@gmail.com>
2024-03-13 17:17:45 +0100

    change dot on the slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -23,46 +23,39 @@ const Slider = () => {
     nextCard();
   });
 
-  const handleChange = (e) => {
-    setIndex(parseInt(e.target.value, 10)); // update pagination
-  };
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        // Adding a div inside the map function to assign a unique ID to each parent element.
-        <div key={event.title}>
-          <div
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
-            </div>
-          </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                 <input              
-                 key={event.id}
-                 type="radio"
-                 name="radio-button"
-                 value={radioIdx}
-                 checked={index === radioIdx}
-                 onChange={handleChange}
-               />
-              ))}
-            </div>
+    {byDateDesc?.map((event, idx) => (
+      <div
+        key={event.title}
+        className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}
+      >
+        <img src={event.cover} alt="forum" />
+        <div className="SlideCard__descriptionContainer">
+          <div className="SlideCard__description">
+            <h3>{event.title}</h3>
+            <p>{event.description}</p>
+            <div>{getMonth(new Date(event.date))}</div>
           </div>
         </div>
-      ))}
+      </div>
+    ))}
+
+    <div className="SlideCard__paginationContainer">
+      <div className="SlideCard__pagination">
+        {byDateDesc?.map((focus, radioIdx) => (
+          <input
+            key={focus.title}
+            type="radio"
+            name="radio-button"
+            checked={index === radioIdx} // index changed
+            readOnly
+          />
+        ))}
+      </div>
     </div>
-  );
+  </div>
+);
 };
 
 export default Slider;
