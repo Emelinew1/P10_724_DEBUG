e3e56e5febd44e311a061bf849d4b53d34f1c8d4
emelinew1 <emeline.wulstecke@gmail.com>
2024-03-22 11:25:31 +0100

    optional tests helpers date

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -1,15 +1,19 @@
 /**
  * 
  */
+import { getMonth } from "./index.js";
 
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
+        it("the function returns 'janvier' for 2022-01-01 as date", () => {
+            const date = new Date("2022-01-01");
+            expect(getMonth(date)).toEqual("janvier");
         });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // 
+
+        it("the function returns 'juillet' for 2022-07-08 as date", () => {
+            const date = new Date("2022-07-01");
+            expect(getMonth(date)).toEqual("juillet");
         });
     });
-})
+});
 
